f4485ebcb494bbc22aa33f474da86c99d742e554
Anna Pastykhova <rainbow261@gmail.com>
2025-04-09 20:11:40 +0300

    hw2

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,14 @@
-function getElementWidth(content, padding, border) {
-    const contentValue = parseFloat(content);
-    const paddingValue = parseFloat(padding);
-    const borderValue = parseFloat(border);
-    
-    return contentValue + 2 * paddingValue + 2 * borderValue;
+function checkForSpam(message) {
+  const lowerCaseMessage = message.toLowerCase();
+  return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(checkForSpam("Latest technology news")); 
+console.log(checkForSpam("JavaScript weekly newsletter"));
+console.log(checkForSpam("Get best sale offers now!"));
+console.log(checkForSpam("Amazing SalE, only tonight!")); 
+console.log(checkForSpam("Trust me, this is not a spam message")); 
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); 
+console.log(checkForSpam("[SPAM] How to earn fast money?")); 
+
+
